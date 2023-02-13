6068a22184dd941b411a2de7bae67a59a02be465
coderman47 <askinglucho@gmail.com>
2023-02-13 12:19:19 -0300

    create: delete product controller && router

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -5,6 +5,7 @@ import {
 	getAllProducts,
 	getProductId,
 	updateProduct,
+	deleteProduct
 } from "../controller/product.controller";
 
 const productRouter = Router();
@@ -13,5 +14,6 @@ productRouter.get("/", getAllProducts);
 productRouter.post("/", createProduct);
 productRouter.get("/:id", getProductId);
 productRouter.put("/:id", updateProduct);
+productRouter.delete("/:id", deleteProduct);
 
 export default productRouter;
